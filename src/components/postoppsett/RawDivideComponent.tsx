1c9ec4a3ccd26a1bdb83c735d874409f0b6120d3
KjetilOverby <kjeoever@msn.com>
2024-10-07 11:46:19 +0200

    egendefinert verdi

--- src/components/postoppsett/RawDivideComponent.tsx
+++ src/components/postoppsett/RawDivideComponent.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-
 import ringlist from "~/utils/ringlist";
 import RingPickerRaw from "./reusable/RingPickerRaw";
-import { get } from "http";
+import { set } from "zod";
+
 type RawDataType = {
   id: number;
   name: string;
@@ -33,6 +33,7 @@ export const RawDivideComponent: React.FC<RawDivideComponentProps> = ({
   setCalculationResult,
 }) => {
   const [getRing, setGetRing] = useState(0);
+  const [ringValue, setRingValue] = useState<number | "">("");
 
   useEffect(() => {
     setCalculationResult(
@@ -43,11 +44,13 @@ export const RawDivideComponent: React.FC<RawDivideComponentProps> = ({
   }, [getRing, shimsVal, firstRingVal, getRawValues]);
 
   const rawInputHandler = () => {
-    setFirstRingVal(getRing);
+    const value = typeof ringValue === "number" ? ringValue : getRing;
+    setFirstRingVal(value);
   };
 
   const skimsHandler = () => {
-    setShimsVal(getRing);
+    const value = typeof ringValue === "number" ? ringValue : getRing;
+    setShimsVal(value);
     setShimsVal2(
       parseFloat(
         (Number(getRawValues) + 1.4 - firstRingVal - shimsVal).toFixed(1),
@@ -55,37 +58,57 @@ export const RawDivideComponent: React.FC<RawDivideComponentProps> = ({
     );
   };
 
-  const nullstillHandler = () => {
-    setFirstRingVal(0);
-    setShimsVal(0);
-    setShimsVal2(0);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    setRingValue(isNaN(value) ? "" : value);
   };
+
+  const ringValueHandler = (value: number) => {
+    setGetRing(value);
+    setRingValue("");
+  };
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0  z-10  h-screen w-screen rounded-xl bg-base-100 p-5">
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-10 h-screen w-screen rounded-xl bg-base-100 p-5">
       <div className="grid h-[98%] grid-cols-2 place-content-end">
         <div className="">
           <div className="mr-10 flex w-20 flex-col">
-            <button onClick={rawInputHandler} className="btn btn-primary mb-2">
+            <button
+              onClick={rawInputHandler}
+              className="btn btn-primary mb-2 text-neutral"
+            >
               RING
             </button>
-            <button onClick={skimsHandler} className="btn btn-primary mb-2">
+            <button
+              onClick={skimsHandler}
+              className="btn btn-primary mb-2 text-neutral"
+            >
               SKIMS
             </button>
-            {/*  <button onClick={nullstillHandler} className="btn btn-primary">
-              Nullstill
-            </button> */}
           </div>
-          <RingPickerRaw
-            values={ringlist}
-            position="right-0"
-            title="Utfylling bak"
-            onChange={(value: number) => setGetRing(value)}
-          />
+          <div>
+            <input
+              type="number"
+              value={ringValue}
+              onChange={handleInputChange}
+              placeholder="Egendefinert verdi"
+              className="mt-4 rounded border p-2"
+            />
+
+            <RingPickerRaw
+              values={ringlist}
+              position="right-0"
+              title="Utfylling bak"
+              onChange={ringValueHandler}
+            />
+          </div>
         </div>
-        <div className="flex w-full  items-end justify-end pr-20">
+        <div className="flex w-full items-end justify-end pr-20">
           <div className="w-96">
             <div className="w-96">
-              <p className="text-5xl text-secondary">Valgt ring: {getRing}</p>
+              <p className="text-5xl text-secondary">
+                Valgt ring: {ringValue !== "" ? ringValue : getRing}
+              </p>
             </div>
           </div>
 
